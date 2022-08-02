59a46f099ec75efe0c2aac45d17f361e8df6dd6c
LucasGuilherm <lucassade110@gmail.com>
2022-08-02 02:18:51 -0300

    Alteracao col-span

--- src/components/itemVisor.tsx
+++ src/components/itemVisor.tsx
@@ -2,13 +2,12 @@ interface Props2 {
   valor: string;
   label: string;
   largura: any;
-  altura: any;
 }
 
 export function ItemVisor(props: Props2) {
   return (
     <div
-      className={`p-2 rounded bg-white flex flex-col col-span-${props.largura} row-span-${props.altura}`}
+      className={`p-2 rounded bg-white flex flex-col col-span-${props.largura}`}
     >
       <label className="text-lg">{props.label}</label>
       <p className="font-bold text-2xl">{props.valor}</p>
