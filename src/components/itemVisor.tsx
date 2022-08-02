ea333dfb998ad627246a66d1c362c67a646ab070
LucasGuilherm <lucassade110@gmail.com>
2022-08-02 13:07:47 -0300

    Ajustado posiçao QTDE

--- src/components/itemVisor.tsx
+++ src/components/itemVisor.tsx
@@ -2,13 +2,27 @@ interface Props2 {
   valor: string;
   label: string;
   largura: string;
+  customStyle?: string;
+  fontSize?: string;
 }
 
 export function ItemVisor(props: Props2) {
+  const defaultStyle = "flex-col";
+
   return (
-    <div className={`p-2 rounded bg-white flex flex-col ${props.largura}`}>
+    <div
+      className={`${
+        props.largura
+      } px-2 py-1 rounded bg-white h-full overflow-hidden whitespace-nowrap flex ${
+        props.customStyle ? props.customStyle : defaultStyle
+      }`}
+    >
       <label className="text-lg">{props.label}</label>
-      <p className="font-bold text-2xl">{props.valor}</p>
+      <p
+        className={`font-bold ${props.fontSize ? props.fontSize : "text-2xl"}`}
+      >
+        {props.valor}
+      </p>
     </div>
   );
 }
--- src/components/visor.tsx
+++ src/components/visor.tsx
@@ -2,13 +2,29 @@ import { ItemVisor } from "./itemVisor";
 
 export function Visor() {
   return (
-    <div className="bg-gray-200 h-auto w-full grid gap-1 p-1 grid-cols-4 grid-rows-3 grid-flow-row">
+    <div className="bg-gray-200 overflow-hidden rounded-t w-full grid gap-1 p-1 grid-cols-4 grid-rows-3 grid-flow-row">
       <ItemVisor largura={"col-span-2"} label="Código" valor="1234" />
       <ItemVisor largura={"col-span-1"} label="Un" valor="UN" />
       <ItemVisor largura={"col-span-1"} label="Preço" valor="2,99" />
-      <ItemVisor largura={"col-span-4"} label="Produto" valor="Leite" />
-      <ItemVisor largura={"col-span-2"} label="Qtde" valor="10" />
-      <ItemVisor largura={"col-span-2"} label="Qtde Ant" valor="3" />
+      <ItemVisor
+        largura={"col-span-4"}
+        label="Produto"
+        valor="Leite Condensado"
+      />
+      <ItemVisor
+        customStyle={"flex-row  justify-between"}
+        fontSize={"text-4xl"}
+        largura={"col-span-2"}
+        label="Qtde Ant"
+        valor="3"
+      />
+      <ItemVisor
+        customStyle={"flex-row  justify-between"}
+        fontSize={"text-4xl"}
+        largura={"col-span-2"}
+        label="Qtde"
+        valor="10"
+      />
     </div>
   );
 }
